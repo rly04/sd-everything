b628ffcde17ccfad8c048f8625c76024ae848fb9
riley earle <rearle1706@gmail.com>
2024-07-05 18:22:57 +0100

    conditionals completed

---
+++ js-conditionals/app.js
@@ -0,0 +1,11 @@
+let age = 14
+let movieRating = 15
+
+if (age > movieRating) {
+    console.log('You are old enough to watch !')}
+    else if (age === movieRating) {
+        console.log("Oh! Just about made it!") }
+        else if (age < movieRating) {
+            console.log("GET OUT! GET OUT NOW! YOU CANT WATCH THIS YET GET OUT!")
+        }
+    
