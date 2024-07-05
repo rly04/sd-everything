94373334b03088d5469b5bd8029ea31772160159
riley earle <rearle1706@gmail.com>
2024-07-05 06:43:00 +0100

    JS variables

---
+++ js-variables/app.js
@@ -0,0 +1,14 @@
+console.log("I know you're watching Tim.")
+
+let riley = "amazing and super cool"
+let tim = "i guess hes smart or whatever."
+
+console.log(riley)
+console.log(tim)
+
+
+let myName = "Riley"
+let age = "20"
+let catName = "Lola"
+
+console.log("Hello, Tim. My name is",myName,"and I am",age,"years old. I also have a cat named",catName)
