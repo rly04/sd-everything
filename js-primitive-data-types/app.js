5d06e08edfc02a170a1b6c1c9c65b62c9b378fb2
riley earle <rearle1706@gmail.com>
2024-07-05 07:40:15 +0100

    way too much for a work shop i am crying

---
+++ js-primitive-data-types/app.js
@@ -0,0 +1,60 @@
+console.log("hi")
+
+// // this is how we do simple maths in js :)
+// let number = 1;
+// console.log(number)
+
+// let vp = 2175
+// let vandal = 1775
+// console.log(vp - vandal)
+
+// // STRINGS
+
+// // let name = 'Riley'
+// // console.log(name)
+
+// let firstName = 'Riley';
+// let lastName = 'Earle';
+// // let fullName = firstName + ' ' + lastName
+// // console.log(fullName)
+
+// // STRING LITERALS
+
+// let fullName = `${firstName} ${lastName}`
+// console.log(fullName)
+
+//BOOLEANS
+
+// let isTrue = true;
+// let isFalse = false;
+// console.log(isTrue)
+// console.log(isFalse)
+
+// let name = 'Riley'
+// console.log(typeof name)
+
+// let isTrue = true
+// console.log(`This type is a ${typeof isTrue}` )  
+
+// COMPARISONS
+
+// let a = 27
+// let b = 27
+// let isAGreaterThanB = a > b
+// let isALessThanB = a < b
+// let isAEqualToB = a === b
+// console.log(isAGreaterThanB)
+// console.log(isALessThanB)
+// console.log(isAEqualToB)
+
+// console.log(`Is ${a} greater than ${b}?`, isAGreaterThanB)
+// console.log(`Is ${a} less than ${b}?`, isALessThanB)
+// console.log(`Is ${a} equal to ${b}?`, isAEqualToB)
+
+let myFavouriteGame = 'Valorant'
+let prices = 'too high'
+let vandal = 30
+
+const sentence = `My favourite game at the moment is ${myFavouriteGame} but I do not like the prices in this game they are way ${prices} one vandal skin costs around ${vandal} pounds!!`
+
+console.log(sentence)
