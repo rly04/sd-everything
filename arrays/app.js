d5896105c33c35ecc961a6cb4086d4c6aee1970b
riley earle <rearle1706@gmail.com>
2024-07-14 16:35:29 +0100

    arrays

---
+++ arrays/app.js
@@ -0,0 +1,40 @@
+console.log("hello eorl")
+
+const blogPosts = [
+    "Number 1",
+    "Number 2",
+    "Number 3",
+]
+
+console.log(blogPosts)
+console.log(blogPosts[0])
+console.log(blogPosts[1])
+console.log(blogPosts[2])
+
+blogPosts[3] = "Number 4"
+
+console.log(blogPosts[3])
+
+const favouriteFood = [
+    "Pizza",
+    "Burger",
+    "Kebab",
+]
+
+console.log(favouriteFood)
+
+const favColors = [
+    "Red",
+    "Blue",
+    "Purple",
+]
+
+console.log(favColors)
+
+const favNumbers = [
+    "17",
+    "23",
+    "29",
+]
+
+console.log(favNumbers)
